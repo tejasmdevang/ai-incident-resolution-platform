51dd71ee4b96366fd51ef39ad98073c9aa36073a
Tejas M Devang <tejasdevangm@gmail.com>
2026-08-01 18:12:53 +0530

    feat: add incident repository

---
+++ src/main/java/com/tejas/incidentplatform/repository/IncidentRepository.java
@@ -0,0 +1,9 @@
+package com.tejas.incidentplatform.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.tejas.incidentplatform.entity.Incident;
+
+
+public interface IncidentRepository extends JpaRepository(Incident, Long) {
+
+}
