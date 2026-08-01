fd6951e501c821f2ba9ccf66e9898342107f1dd2
Tejas M Devang <tejasdevangm@gmail.com>
2026-08-01 18:47:36 +0530

    feat: add incident request DTO

---
+++ src/main/java/com/tejas/incidentplatform/dto/IncidentRequest.java
@@ -0,0 +1,27 @@
+package com.tejas.incidentplatform.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class IncidentRequest {
+
+    @NotNull
+    private String title;
+
+    @NotBlank
+    private String description;
+
+    @NotBlank
+    private String serviceName;
+
+    @NotNull
+    private Severity severity;
+
+}
